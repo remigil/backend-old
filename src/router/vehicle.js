6b986ccad5dee8a02a1580d54276d2cd80d180f5
K3I <k3i@192.168.1.5>
2022-08-18 22:34:54 +0700

    vechile

--- src/router/vehicle.js
+++ src/router/vehicle.js
@@ -1,17 +1,19 @@
 const router = require("express").Router();
 const { body, param } = require("express-validator");
 const UserController = require("../controller/user");
+const VehicleController = require("../controller/vehicle");
 const authMiddleware = require("../middleware/authentication");
 const form_validation = require("../middleware/form_validation");
 router.get(
   "/get",
   // authMiddleware.jwtAuth,
-  UserController.getLoggedUser
+  VehicleController.get
+  // UserController.getLoggedUser
 );
 router.post(
   "/add",
-  authMiddleware.jwtAuth, 
-  form_validation, 
-  UserController.add
+  authMiddleware.jwtAuth,
+  form_validation,
+  VehicleController.get
 );
 module.exports = router;
