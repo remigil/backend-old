73d8bc8746ed783682741349edf4f6975dce3151
K3I <k3i@192.168.1.5>
2022-08-20 00:45:09 +0700

    login account for mobile

--- src/controller/authentication.js
+++ src/controller/authentication.js
@@ -5,6 +5,7 @@ const { JWTEncrypt, JWTVerify, AESDecrypt } = require("../lib/encryption");
 const moment = require("moment");
 const TokenTrackNotif = require("../model/token_track_notif");
 const Officer = require("../model/officer");
+const Account = require("../model/account");
 
 class Authentication {
   static login = async (req, res) => {
@@ -28,64 +29,73 @@ class Authentication {
     response(res, false, "Login failed, please try again!", null, 401);
   };
   static loginMobile = async (req, res) => {
-    const team = await Officer.findOne({
-      where: {
-        team: req.body.team,
-      },
-    });
+    try {
+      const account = await Account.findOne({
+        where: {
+          id_account: req.body.team,
+        },
+      });
 
-    const officer = await Officer.findOne({
-      where: {
-        nrp_user: req.body.team,
-      },
-    });
+      const officer = await Officer.findOne({
+        where: {
+          nrp_officer: req.body.nrp_user,
+        },
+      });
 
-    if (!officer) {
-      return response(res, false, "Akun Anda Tidak Terdaftar di database kami");
-    }
-
-    let nrp_user = await TokenTrackNotif.findOne({
-      where: {
-        nrp_user: officer.nrp_user,
-        user_id: AESDecrypt(team.id, {
-          isSafeUrl: true,
-          parseMode: "string",
-        }),
-      },
-    });
-    if (nrp_user) {
-      if (nrp_user.device_user != req.body.device_user) {
+      if (!officer) {
         return response(
           res,
           false,
-          "Data Anda Telah ada di device lainnya, silahkan login menggunakan device sebelumnya"
+          "Akun Anda Tidak Terdaftar di database kami"
         );
       }
-    } else {
-      nrp_user = (
-        await TokenTrackNotif.create({
-          nrp_user: officer.nrp_user,
-          device_user: req.body.device_user,
-          team_id: AESDecrypt(team.id, {
+
+      let nrp_user = await TokenTrackNotif.findOne({
+        where: {
+          nrp_user: officer.nrp_officer,
+          team_id: AESDecrypt(account.id, {
             isSafeUrl: true,
             parseMode: "string",
           }),
-        })
-      ).dataValues;
-    }
-    if (team) {
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        const accessToken = JWTEncrypt({
-          uid: team.id,
-          nrp_user: nrp_user.id,
-          timestamp: moment().unix(),
-        });
-        return response(res, true, "Login succeed", {
-          accessToken,
-        });
+        },
+      });
+      if (nrp_user) {
+        if (nrp_user.device_user != req.body.device_user) {
+          return response(
+            res,
+            false,
+            "Data Anda Telah ada di device lainnya, silahkan login menggunakan device sebelumnya"
+          );
+        }
+      } else {
+        nrp_user = (
+          await TokenTrackNotif.create({
+            nrp_user: officer.nrp_officer,
+            device_user: req.body.device_user,
+            team_id: AESDecrypt(account.id, {
+              isSafeUrl: true,
+              parseMode: "string",
+            }),
+          })
+        ).dataValues;
       }
+      if (account) {
+        if (bcrypt.compareSync(req.body.password, account.password)) {
+          const accessToken = JWTEncrypt({
+            uid: account.id,
+            nrp_user: nrp_user.id,
+            officer: officer.id,
+            timestamp: moment().unix(),
+          });
+          return response(res, true, "Login succeed", {
+            accessToken,
+          });
+        }
+      }
+      response(res, false, "Login failed, please try again!", nrp_user, 401);
+    } catch (error) {
+      response(res, false, "Login failed, please try again!", error.message);
     }
-    response(res, false, "Login failed, please try again!", null, 401);
   };
   static validateLogin = async (req, res) => {
     try {
