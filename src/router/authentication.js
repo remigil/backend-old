894c50b2cd5fbb8f0d4e01a19bf3b0f440ae498d
gutaam.gt <gutaam.gt@gmail.com>
2022-08-27 16:57:18 +0700

    akun

--- src/router/authentication.js
+++ src/router/authentication.js
@@ -6,14 +6,14 @@ const authMiddleware = require("../middleware/authentication");
 router.post(
   "/login",
   body("username").notEmpty().isLength({ min: 3 }),
-  body("password").notEmpty().isLength({ min: 3 }),
+  body("password").notEmpty().isLength({ min: 1 }),
   formValidation,
   Authentication.login
 );
 router.post(
   "/login-mobile",
   body("team").notEmpty().isLength({ min: 3 }),
-  body("password").notEmpty().isLength({ min: 3 }),
+  body("password").notEmpty().isLength({ min: 1 }),
   body("nrp_user").notEmpty().isLength({ min: 1 }),
   formValidation,
   Authentication.loginMobile
