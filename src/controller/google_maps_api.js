3306a0601da0346d7f8494980b94d60738315547
K3I <k3i@K3Is-MacBook-Pro.local>
2022-09-10 20:51:17 +0700

    handling error google maps

--- src/controller/google_maps_api.js
+++ src/controller/google_maps_api.js
@@ -28,124 +28,132 @@ class GoogleAPIs {
       });
   }
   static async directionAPICostumize(req, res) {
-    const { coordinate, alternatif1, alternatif2 } = req.query;
-    if (!coordinate) {
-      return response(res, false, "coordinate Tidak boleh kosong", [], 400);
-    }
-    let dataCoordinate = "";
-    let dataCoordinateAlter2 = "";
-    let dataCoordinateAlter1 = "";
-    let responseAlter1 = {};
-    let responseAlter2 = {};
-    coordinate.forEach((coordinateData, index) => {
-      dataCoordinate += coordinateData;
-      dataCoordinate += index != coordinate.length - 1 ? ";" : "";
-    });
-
-    if (alternatif1) {
-      alternatif1.forEach((coordinateData, index) => {
-        dataCoordinateAlter1 += coordinateData;
-        dataCoordinateAlter1 += index != coordinate.length - 1 ? ";" : "";
+    try {
+      const { coordinate, alternatif1, alternatif2 } = req.query;
+      if (!coordinate) {
+        return response(res, false, "coordinate Tidak boleh kosong", [], 400);
+      }
+      let dataCoordinate = "";
+      let dataCoordinateAlter2 = "";
+      let dataCoordinateAlter1 = "";
+      let responseAlter1 = {};
+      let responseAlter2 = {};
+      coordinate.forEach((coordinateData, index) => {
+        dataCoordinate += coordinateData;
+        dataCoordinate += index != coordinate.length - 1 ? ";" : "";
       });
-    }
-    if (alternatif2) {
-      alternatif2.forEach((coordinateData, index) => {
-        dataCoordinateAlter2 += coordinateData;
-        dataCoordinateAlter2 += index != coordinate.length - 1 ? ";" : "";
-      });
-    }
-    let data = await axios({
-      url:
-        "https://router.project-osrm.org/route/v1/car/" +
-        dataCoordinate +
-        "?overview=simplified&geometries=geojson&alternatives=3&steps=true",
-    });
 
-    if (dataCoordinateAlter1 != "") {
-      responseAlter1 = await axios({
+      if (alternatif1) {
+        alternatif1.forEach((coordinateData, index) => {
+          dataCoordinateAlter1 += coordinateData;
+          dataCoordinateAlter1 += index != coordinate.length - 1 ? ";" : "";
+        });
+      }
+      if (alternatif2) {
+        alternatif2.forEach((coordinateData, index) => {
+          dataCoordinateAlter2 += coordinateData;
+          dataCoordinateAlter2 += index != coordinate.length - 1 ? ";" : "";
+        });
+      }
+      let data = await axios({
         url:
           "https://router.project-osrm.org/route/v1/car/" +
-          dataCoordinateAlter1 +
+          dataCoordinate +
           "?overview=simplified&geometries=geojson&alternatives=3&steps=true",
       });
+
+      if (dataCoordinateAlter1 != "") {
+        responseAlter1 = await axios({
+          url:
+            "https://router.project-osrm.org/route/v1/car/" +
+            dataCoordinateAlter1 +
+            "?overview=simplified&geometries=geojson&alternatives=3&steps=true",
+        });
+      }
+      if (dataCoordinateAlter2 != "") {
+        responseAlter2 = await axios({
+          url:
+            "https://router.project-osrm.org/route/v1/car/" +
+            dataCoordinateAlter2 +
+            "?overview=simplified&geometries=geojson&alternatives=3&steps=true",
+        });
+      }
+
+      let directions = [];
+      let directionsAlter1 = [];
+      let directionsAlter2 = [];
+
+      let getDataRoutes = data.data.routes[0].legs;
+      let getDataRoutesAlter1 = responseAlter1?.data?.routes[0]?.legs;
+      let getDataRoutesAlter2 = responseAlter2?.data?.routes[0]?.legs;
+      getDataRoutes.forEach((leg) => {
+        leg.steps.forEach((step, j) => {
+          step.geometry.coordinates.forEach((intersection, k) => {
+            directions.push({
+              latitude: intersection[1],
+              longitude: intersection[0],
+            });
+          });
+        });
+      });
+      getDataRoutesAlter1?.forEach((leg) => {
+        leg?.steps.forEach((step, j) => {
+          step?.geometry?.coordinates?.forEach((intersection, k) => {
+            directionsAlter1.push({
+              latitude: intersection[1],
+              longitude: intersection[0],
+            });
+          });
+        });
+      });
+      getDataRoutesAlter2?.forEach((leg) => {
+        leg?.steps.forEach((step, j) => {
+          step?.geometry?.coordinates?.forEach((intersection, k) => {
+            directionsAlter2.push({
+              latitude: intersection[1],
+              longitude: intersection[0],
+            });
+          });
+        });
+      });
+
+      return response(
+        res,
+        false,
+        "Succ",
+        {
+          route: directions,
+          distanceKmRoute:
+            (data.data.routes[0].distance / 1000).toFixed(2) + " Km",
+          durationRoute:
+            (data.data.routes[0].duration / 60).toFixed(2) + " Menit",
+          distanceKmRouteAlter1: responseAlter1?.data?.routes[0].distance
+            ? (responseAlter1?.data?.routes[0].distance / 1000).toFixed(2) +
+              " Km"
+            : "0 Km",
+          durationRouteAlter1: responseAlter1?.data?.routes[0].duration
+            ? (responseAlter1?.data?.routes[0].duration / 60).toFixed(2) +
+              " Menit"
+            : "0 Menit",
+          distanceKmRouteAlter2: responseAlter2?.data?.routes[0].distance
+            ? (responseAlter2?.data?.routes[0].distance / 1000).toFixed(2) +
+              " Km"
+            : "0 Km",
+          durationRouteAlter2: responseAlter2?.data?.routes[0].duration
+            ? (responseAlter2?.data?.routes[0].duration / 60).toFixed(2) +
+              " Menit"
+            : "0 Menit",
+          alternatif1: directionsAlter1,
+          alternatif2: directionsAlter2,
+          data: data.data,
+        },
+        200
+      );
+    } catch (error) {
+      console.log({ error });
+      // return response(res, true, "success", data, 200);
+      res.json({ err: error.message });
     }
-    if (dataCoordinateAlter2 != "") {
-      responseAlter2 = await axios({
-        url:
-          "https://router.project-osrm.org/route/v1/car/" +
-          dataCoordinateAlter2 +
-          "?overview=simplified&geometries=geojson&alternatives=3&steps=true",
-      });
-    }
-
-    let directions = [];
-    let directionsAlter1 = [];
-    let directionsAlter2 = [];
-
-    let getDataRoutes = data.data.routes[0].legs;
-    let getDataRoutesAlter1 = responseAlter1?.data?.routes[0]?.legs;
-    let getDataRoutesAlter2 = responseAlter2?.data?.routes[0]?.legs;
-    getDataRoutes.forEach((leg) => {
-      leg.steps.forEach((step, j) => {
-        step.geometry.coordinates.forEach((intersection, k) => {
-          directions.push({
-            latitude: intersection[1],
-            longitude: intersection[0],
-          });
-        });
-      });
-    });
-    getDataRoutesAlter1?.forEach((leg) => {
-      leg?.steps.forEach((step, j) => {
-        step?.geometry?.coordinates?.forEach((intersection, k) => {
-          directionsAlter1.push({
-            latitude: intersection[1],
-            longitude: intersection[0],
-          });
-        });
-      });
-    });
-    getDataRoutesAlter2?.forEach((leg) => {
-      leg?.steps.forEach((step, j) => {
-        step?.geometry?.coordinates?.forEach((intersection, k) => {
-          directionsAlter2.push({
-            latitude: intersection[1],
-            longitude: intersection[0],
-          });
-        });
-      });
-    });
-
-    return response(
-      res,
-      false,
-      "Succ",
-      {
-        route: directions,
-        distanceKmRoute:
-          (data.data.routes[0].distance / 1000).toFixed(2) + " Km",
-        durationRoute:
-          (data.data.routes[0].duration / 60).toFixed(2) + " Menit",
-        distanceKmRouteAlter1: responseAlter1?.data?.routes[0].distance
-          ? (responseAlter1?.data?.routes[0].distance / 1000).toFixed(2) + " Km"
-          : "0 Km",
-        durationRouteAlter1: responseAlter1?.data?.routes[0].duration
-          ? (responseAlter1?.data?.routes[0].duration / 60).toFixed(2) +
-            " Menit"
-          : "0 Menit",
-        distanceKmRouteAlter2: responseAlter2?.data?.routes[0].distance
-          ? (responseAlter2?.data?.routes[0].distance / 1000).toFixed(2) + " Km"
-          : "0 Km",
-        durationRouteAlter2: responseAlter2?.data?.routes[0].duration
-          ? (responseAlter2?.data?.routes[0].duration / 60).toFixed(2) +
-            " Menit"
-          : "0 Menit",
-        alternatif1: directionsAlter1,
-        alternatif2: directionsAlter2,
-        data: data.data,
-      },
-      200
-    );
   }
   static async directionAPIfromOSRM(req, res) {
     try {
