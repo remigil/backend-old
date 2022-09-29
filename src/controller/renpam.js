fc6b35090d0f613c49201716616602d0117b2b44
gutaam.gt <gutaam.gt@gmail.com>
2022-09-29 23:01:30 +0700

    ido

--- src/controller/renpam.js
+++ src/controller/renpam.js
@@ -677,28 +677,28 @@ module.exports = class RenpamController {
       })
         .then(async (op) => {
           if (fieldValue["accounts"] && fieldValue["accounts"].length > 0) {
-            for (let i = 0; i < fieldValue["accounts"].length; i++) {
-              fieldValueAccount = {};
-              fieldValueAccount["renpam_id"] = AESDecrypt(req.params.id, {
-                isSafeUrl: true,
-                parseMode: "string",
-              });
-              fieldValueAccount["account_id"] = AESDecrypt(
-                fieldValue["accounts"][i],
-                {
+            Renpam.findOne({
+              where: {
+                id: AESDecrypt(req.params.id, {
                   isSafeUrl: true,
                   parseMode: "string",
-                }
-              );
-
-              Renpam.findOne({
-                where: {
-                  id: AESDecrypt(req.params.id, {
+                }),
+              },
+            }).then((dataRenpam) => {
+              for (let i = 0; i < fieldValue["accounts"].length; i++) {
+                fieldValueAccount = {};
+                fieldValueAccount["renpam_id"] = AESDecrypt(req.params.id, {
+                  isSafeUrl: true,
+                  parseMode: "string",
+                });
+                fieldValueAccount["account_id"] = AESDecrypt(
+                  fieldValue["accounts"][i],
+                  {
                     isSafeUrl: true,
                     parseMode: "string",
-                  }),
-                },
-              }).then((dataRenpam) => {
+                  }
+                );
+
                 TokenTrackNotif.findAll({
                   where: {
                     team_id: AESDecrypt(fieldValue["accounts"][i], {
@@ -713,30 +713,33 @@ module.exports = class RenpamController {
                         nrp_officer: iterator.nrp_user,
                       },
                     }).then(async (dataOffice) => {
-                      NotifikasiController.singleGlobal({
-                        deepLink: notifHandler.mobile.instruksi + req.params.id,
-                        type: "instruksi",
-                        title: `Instruksi Kakor - ${dataRenpam["name_renpam"]}`,
-                        description: `${req.body["note_kakor"]}`,
-                        officer_id: AESDecrypt(dataOffice.id, {
-                          isSafeUrl: true,
-                          parseMode: "string",
-                        }),
-                        mobile: notifHandler.mobile.instruksi + req.params.id,
-                        web: notifHandler.web.instruksi + req.params.id,
-                        to: iterator.token_fcm,
-                      })
-                        .then((successData) => {
-                          console.log({ successData });
+                      if (dataOffice) {
+                        NotifikasiController.singleGlobal({
+                          deepLink:
+                            notifHandler.mobile.instruksi + req.params.id,
+                          type: "instruksi",
+                          title: `Instruksi Kakor - ${dataRenpam["name_renpam"]}`,
+                          description: `${req.body["note_kakor"]}`,
+                          officer_id: AESDecrypt(dataOffice.id, {
+                            isSafeUrl: true,
+                            parseMode: "string",
+                          }),
+                          mobile: notifHandler.mobile.instruksi + req.params.id,
+                          web: notifHandler.web.instruksi + req.params.id,
+                          to: iterator.token_fcm,
                         })
-                        .catch((errorData) => {
-                          console.log({ errorData });
-                        });
+                          .then((successData) => {
+                            console.log({ successData });
+                          })
+                          .catch((errorData) => {
+                            console.log({ errorData });
+                          });
+                      }
                     });
                   }
                 });
-              });
-            }
+              }
+            });
           }
 
           await transaction.commit();
