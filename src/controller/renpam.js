669df547beb5cc074688283a5f23f3701f5583e1
gutaam.gt <gutaam.gt@gmail.com>
2022-10-17 05:43:06 +0700

    ido

--- src/controller/renpam.js
+++ src/controller/renpam.js
@@ -1226,42 +1226,42 @@ module.exports = class RenpamController {
                 }
               );
 
-              // TokenTrackNotif.findAll({
-              //   where: {
-              //     team_id: AESDecrypt(fieldValue["accounts"][i], {
-              //       isSafeUrl: true,
-              //       parseMode: "string",
-              //     }),
-              //   },
-              // }).then((dataTrack) => {
-              //   for (const iterator of dataTrack) {
-              //     Officer.findOne({
-              //       where: {
-              //         nrp_officer: iterator.nrp_user,
-              //       },
-              //     }).then(async (dataOffice) => {
-              //       NotifikasiController.singleGlobal({
-              //         deepLink: notifHandler.mobile.instruksi + op.id,
-              //         type: "instruksi",
-              //         title: "Edit Instruksi",
-              //         description: op.name_renpam,
-              //         officer_id: AESDecrypt(dataOffice.id, {
-              //           isSafeUrl: true,
-              //           parseMode: "string",
-              //         }),
-              //         mobile: notifHandler.mobile.instruksi + op.id,
-              //         web: notifHandler.mobile.instruksi + op.id,
-              //         to: iterator.token_fcm,
-              //       })
-              //         .then((successData) => {
-              //           console.log({ successData });
-              //         })
-              //         .catch((errorData) => {
-              //           console.log({ errorData });
-              //         });
-              //     });
-              //   }
-              // });
+              TokenTrackNotif.findAll({
+                where: {
+                  team_id: AESDecrypt(fieldValue["accounts"][i], {
+                    isSafeUrl: true,
+                    parseMode: "string",
+                  }),
+                },
+              }).then((dataTrack) => {
+                for (const iterator of dataTrack) {
+                  Officer.findOne({
+                    where: {
+                      nrp_officer: iterator.nrp_user,
+                    },
+                  }).then(async (dataOffice) => {
+                    NotifikasiController.singleGlobal({
+                      deepLink: notifHandler.mobile.instruksi + op.id,
+                      type: "instruksi",
+                      title: "Edit Instruksi",
+                      description: op.name_renpam,
+                      officer_id: AESDecrypt(dataOffice.id, {
+                        isSafeUrl: true,
+                        parseMode: "string",
+                      }),
+                      mobile: notifHandler.mobile.instruksi + op.id,
+                      web: notifHandler.mobile.instruksi + op.id,
+                      to: iterator.token_fcm,
+                    })
+                      .then((successData) => {
+                        console.log({ successData });
+                      })
+                      .catch((errorData) => {
+                        console.log({ errorData });
+                      });
+                  });
+                }
+              });
 
               // if (dataRenAc && dataRenVip) {
               await RenpamAccount.destroy({
