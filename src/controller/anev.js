43590e9a9cd8260d52e9bcf0f1512504a318700e
danumz <maulanazakaria.danu@gmail.com>
2022-10-31 19:01:13 +0800

    test

--- src/controller/anev.js
+++ src/controller/anev.js
@@ -1,15 +1,7 @@
 const response = require("../lib/response");
-const User = require("../model/user");
-const bcrypt = require("bcrypt");
-const { JWTEncrypt, JWTVerify, AESDecrypt } = require("../lib/encryption");
+
 const moment = require("moment");
-const TokenTrackNotif = require("../model/token_track_notif");
-const Officer = require("../model/officer");
-const Account = require("../model/account");
-const fs = require("fs");
-const { default: readXlsxFile } = require("read-excel-file/node");
-const { replace } = require("lodash");
-const ReportLogin = require("../model/reportLogin");
+
 const puppeteer = require("puppeteer");
 const path = require("path");
 module.exports = class Anev {
