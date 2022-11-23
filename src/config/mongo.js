0f9863d7e3c7bf587e11d5b36c1ca0af8dfbee60
danumz <maulanazakaria.danu@gmail.com>
2022-11-23 21:33:15 +0700

    baru

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const dbCon = `mongodb://k3iadminmongo:k3imongo@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?retryWrites=true&w=majority`;
+const dbCon = `mongodb://k3iadminmongo:k3imongodb@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?retryWrites=true&w=majority`;
 
 const mongo = mongoose
   .connect(dbCon, { useNewUrlParser: true, useUnifiedTopology: true })
