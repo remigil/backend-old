b648f4447cd2fab41b2550c85fde819a382bf8a9
gutaam.gt <gutaam.gt@gmail.com>
2023-01-05 22:04:40 +0700

    ido

--- src/controller/etilang_perkara.js
+++ src/controller/etilang_perkara.js
@@ -28,12 +28,83 @@ const fieldData = {
 module.exports = class EtilangPerkaraController {
   static get = async (req, res) => {
     try {
-      const data = await Etilang_perkara.findAll();
-      response(res, true, "Succeed", {
-        data,
-        recordsFiltered: 0,
-        recordsTotal: 0,
-      });
+      const {
+        length = 10,
+        start = 0,
+        serverSide = null,
+        search = null,
+        filter = [],
+        filterSearch = [],
+        order = null,
+        orderDirection = "asc",
+      } = req.query;
+      const modelAttr = Object.keys(Etilang_perkara.getAttributes());
+      let getData = { where: null };
+      if (serverSide?.toLowerCase() === "true") {
+        const resPage = pagination.getPagination(length, start);
+        getData.limit = resPage.limit;
+        getData.offset = resPage.offset;
+      }
+      // getDataRules.order = [[modelAttr[order], orderDirection.toUpperCase()]];
+      getData.order = [
+        [
+          order != null ? order : "no_bayar",
+          orderDirection != null ? orderDirection : "asc",
+        ],
+      ];
+      if (search != null) {
+        let whereBuilder = [];
+        modelAttr.forEach((key) => {
+          whereBuilder.push(
+            Sequelize.where(
+              Sequelize.fn(
+                "lower",
+                Sequelize.cast(Sequelize.col(key), "varchar")
+              ),
+              {
+                [Op.like]: `%${search.toLowerCase()}%`,
+              }
+            )
+          );
+        });
+        getData.where = {
+          [Op.or]: whereBuilder,
+        };
+      }
+      if (
+        filter != null &&
+        filter.length > 0 &&
+        filterSearch != null &&
+        filterSearch.length > 0
+      ) {
+        const filters = [];
+        filter.forEach((fKey, index) => {
+          if (_.includes(modelAttr, fKey)) {
+            filters[fKey] = filterSearch[index];
+          }
+        });
+        getData.where = {
+          ...getData.where,
+          ...filters,
+        };
+      }
+
+      Etilang_perkara.findAll({
+        ...getData,
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      })
+        .then((result) => {
+          return response(res, true, "Succeed", {
+            result,
+            recordsFiltered: 0,
+            recordsTotal: 0,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     } catch (e) {
       response(res, false, "Failed", e.message);
     }
--- src/model/etilang_perkara.js
+++ src/model/etilang_perkara.js
@@ -4,7 +4,21 @@ const Model = Sequelize.Model;
 
 class Etilang_perkara extends Model {}
 Etilang_perkara.init(
-  {},
+  {
+    no_bayar: {
+      type: Sequelize.STRING(50),
+      primaryKey: true,
+    },
+    kepolisian_induk: {
+      type: Sequelize.STRING(100),
+    },
+    tgl_perkara: {
+      type: Sequelize.INTEGER,
+    },
+    updated_on: {
+      type: Sequelize.INTEGER,
+    },
+  },
   {
     defaultScope: {
       // where: Sequelize.literal("eliminasi.deleteEliminasi is null"),
